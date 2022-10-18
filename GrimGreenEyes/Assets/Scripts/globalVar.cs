eb3320fb0291e58eb6d62cab0cdf9a9b1bdf1f96
AlvaroS11 <alvarogarcia111@gmail.com>
2022-10-18 18:36:34 +0200

    Final merge?

--- GrimGreenEyes/Assets/Scripts/globalVar.cs
+++ GrimGreenEyes/Assets/Scripts/globalVar.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HardThorns : Skill
+public class globalVar : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public int level;
     void Start()
     {
-        isActiveSkill = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
