e31510cff3999c9047176b9259f98cdafa027a5c
baldo3 <baldorodriguezarbol@gmail.com>
2022-11-13 20:15:38 +0100

    tutorial book add

---
+++ GrimGreenEyes/Assets/Book.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Book : MonoBehaviour
+{
+    [SerializeField] public List<BookPage> pageList = new List<BookPage>();
+    public int position = 0;
+    [SerializeField] private GameObject nextButton, prevButton, closeButton;
+    [SerializeField] private TextMeshProUGUI titleText, descriptionText;
+    [SerializeField] private Image pageImage;
+
+    public void OpenBook()
+    {
+        gameObject.SetActive(true);
+        position = 0;
+        UpdateVisuals();
+    }
+
+    public void CloseBook()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void NextPage()
+    {
+        position++;
+        UpdateVisuals();
+    }
+
+    public void PrevPage()
+    {
+        position--;
+        UpdateVisuals();
+    }
+
+    private void UpdateVisuals()
+    {
+        nextButton.SetActive(position < pageList.Count-1);
+        prevButton.SetActive(position > 0);
+        closeButton.SetActive(!(position < pageList.Count - 1));
+        titleText.text = pageList[position].title;
+        descriptionText.text = pageList[position].description;
+        pageImage.sprite = pageList[position].image;
+    }
+}
+
+
+[System.Serializable]
+public class BookPage
+{
+    public string title;
+    [TextArea(3, 6)]
+    public string description;
+    public Sprite image;
+}
