fba99d0a38bee4d453c844dd5ff30ed4291e9d58
dieguoin <dieguoin.developer@gmail.com>
2022-12-10 05:33:18 +0100

    Animacion skill arania aniadidas

--- GrimGreenEyes/Assets/Scripts/Combat/Skills/Insects/Toxines.cs
+++ GrimGreenEyes/Assets/Scripts/Combat/Skills/Insects/Toxines.cs
@@ -8,10 +8,14 @@ public class Toxines : Skill
     {
         if(enemy.tag == "Enemy")
         {
+            GameObject animation = Instantiate(attackAnimation, enemy.transform);
+            animation.GetComponent<AttackAnimation>().Animate(0);
             enemy.GetComponent<Entity>().Heal((int)((float)enemy.GetComponent<Entity>().maxLivePoints * 0.3f));
         }
         if(enemy.tag == "Player")
         {
+            GameObject animation = Instantiate(attackAnimation, enemy.transform);
+            animation.GetComponent<AttackAnimation>().Animate(6);
             enemy.GetComponent<Entity>().poisoned = true;
         }
         DeactivateSkill(player.GetComponent<Entity>());
